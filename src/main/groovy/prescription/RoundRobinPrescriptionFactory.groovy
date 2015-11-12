0028e0165283abb72d4bd378c3a6a7d164b1502c
Justinian Oprescu <ojusti@yahoo.fr>
2015-11-12 15:35:14 +0200

    groovish

--- src/main/groovy/prescription/RoundRobinPrescriptionFactory.groovy
+++ src/main/groovy/prescription/RoundRobinPrescriptionFactory.groovy
@@ -2,44 +2,41 @@ package prescription
 
 import prescription.spec.EmptyPrescriptionException
 
-class RoundRobinPrescriptionFactory {
-    def drugList = []
+import static java.lang.Math.ceil
 
-    def makePrescription() {
-        if(drugList.isEmpty()) {
+class RoundRobinPrescriptionFactory {
+    def specification
+
+    def makePrescription(drugList) {
+        if(drugList == null || drugList.isEmpty()) {
             throw new EmptyPrescriptionException()
         }
 
-        def prescription = new Prescription()
-        separateListsOfDrugsForReceipts().each { prescription.addReceiptFor(it) }
-        return prescription
+        separateListsOfDrugsForEachReceipt(drugList).inject(new Prescription()) { prescription, listOfDrugsForReceipt -> prescription.addReceiptFor(listOfDrugsForReceipt) }
     }
 
-    private def separateListsOfDrugsForReceipts() {
-        def receipts = makeEmptyReceipts(computeReceiptCount())
-        def sequenceOfOneBoxDrug = drugList.collect { separateBoxes(it) }.flatten()
+    private def separateListsOfDrugsForEachReceipt(drugList) {
+        def receipts = makeEmptyReceipts(computeReceiptCount(drugList))
+        def sequenceOfOneBoxDrugs = drugList.collect { separateBoxes(it) }.flatten()
 
-        for (int receiptIndex = 0; !sequenceOfOneBoxDrug.isEmpty(); receiptIndex = (receiptIndex + 1) % receipts.size()) {
-
-            receipts[receiptIndex] << sequenceOfOneBoxDrug.remove(0)
-        }
+        sequenceOfOneBoxDrugs.inject(0) { index, drugBox ->  receipts[index] << drugBox; (index + 1) % receipts.size() }
         return receipts.values()
     }
 
     private def makeEmptyReceipts(def count) {
-        Map receipts = [:]
-        (0..count-1).each { receipts.put(it, []) }
-        return receipts
+        (0..count-1).collectEntries {[(it): []]}
     }
 
     private def separateBoxes(def drug) {
-        def boxes = []
-        (1..drug.boxCount).each { boxes << new Drug(name: drug.name, boxCount: 1) }
-        return boxes
+        (1..drug.boxCount).collect { new Drug(name: drug.name, boxCount: 1) }
     }
 
-    private def computeReceiptCount() {
-        (int) [drugList.boxCount.max(), Math.ceil(drugList.boxCount.sum() / 10)].max()
+    private def computeReceiptCount(drugList) {
+        def receiptSpec = specification.receiptSpec
+        def drugSpec = receiptSpec.drugSpec
+        def minNumberOfReceipts = ceil(ceil(drugList.boxCount.sum() / drugSpec.maxBoxCount) / receiptSpec.maxSize)
+        def minNumberOfReceiptsForOneDrug = ceil(drugList.boxCount.max() / drugSpec.maxBoxCount)
+        [minNumberOfReceiptsForOneDrug, minNumberOfReceipts].max()
     }
 
 }
--- src/test/groovy/prescription/RoundRobinPrescriptionTest.groovy
+++ src/test/groovy/prescription/RoundRobinPrescriptionTest.groovy
@@ -12,16 +12,18 @@ import spock.lang.Specification
 class RoundRobinPrescriptionTest extends Specification {
 
     def generator
+    def belgianSpec
+    def factory
 
     def setup() {
         generator = new DrugsGenerator()
+        belgianSpec = new PrescriptionSpecification(receiptSpec: new ReceiptSpecification(maxSize: 10, drugSpec: new DrugSpecification(maxBoxCount: 1)))
+        factory = new RoundRobinPrescriptionFactory(specification: belgianSpec)
     }
 
     void "should distribute 0 drugs to 0 receipts"() {
-        given:
-        def distributor = new RoundRobinPrescriptionFactory()
         when:
-        distributor.makePrescription()
+        factory.makePrescription()
         then:
         thrown(EmptyPrescriptionException)
     }
@@ -29,35 +31,27 @@ class RoundRobinPrescriptionTest extends Specification {
     void "should distribute 1 box of 1 drug to 1 receipt"() {
         given:
         def drugList = [drug(1)]
-        def factory = new RoundRobinPrescriptionFactory(drugList: drugList)
         expect:
-        new Prescription().addReceiptFor(drugList) == factory.makePrescription()
+        new Prescription().addReceiptFor(drugList) == factory.makePrescription(drugList)
     }
 
     void "should distribute 2 boxes of 1 drug to 2 receipts"() {
-        given:
-        def factory = new RoundRobinPrescriptionFactory(drugList: [drug('a', 2)])
         expect:
         new Prescription().addReceiptFor([drug('a', 1)])
-                .addReceiptFor([drug('a', 1)]) == factory.makePrescription()
+                .addReceiptFor([drug('a', 1)]) == factory.makePrescription([drug('a', 2)])
     }
 
     void "should distribute 2 boxes of 2 drugs to 2 receipts"() {
-        given:
-        def factory = new RoundRobinPrescriptionFactory(drugList: [drug('a', 2), drug('b', 2)])
         expect:
         new Prescription().addReceiptFor([drug('a', 1), drug('b', 1)])
-                .addReceiptFor([drug('a', 1), drug('b', 1)]) == factory.makePrescription()
+                .addReceiptFor([drug('a', 1), drug('b', 1)]) == factory.makePrescription([drug('a', 2), drug('b', 2)])
     }
 
     void "should distribute 10 boxes of 10 drugs and 2 boxes of another to 11 receipts"() {
         given:
         def longDrugList = [drug('a', 10), drug('b', 10), drug('c', 10), drug('d', 10), drug('e', 10), drug('f', 10), drug('g', 10), drug('h', 10), drug('i', 10), drug('j', 10), drug('k', 2)]
-        def factory = new RoundRobinPrescriptionFactory(drugList: longDrugList)
-        when:
-        def spec = new PrescriptionSpecification(receiptSpec: new ReceiptSpecification(maxSize: 10, drugSpec: new DrugSpecification(maxBoxCount: 1)))
-        then:
-        spec.test(factory.makePrescription())
+        expect:
+        belgianSpec.test(factory.makePrescription(longDrugList))
     }
 
     def drug(boxesCount) {
